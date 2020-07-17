8771416d91292ba85960e8285ed1aa28888c8902
FabioFleitas <fabio@tesorio.com>
2020-07-16 17:24:24 -0700

    Updated proxy headers

--- src/constants.py
+++ src/constants.py
@@ -24,6 +24,11 @@ http <
 
         location / <
             proxy_pass http://main;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header Host $http_host;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Forwarded-Host $host;
         >
     >
 
